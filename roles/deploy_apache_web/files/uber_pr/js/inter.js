b2032eed9682eecb730dd5b147657a9e2cd9ac6b
Vova <vova234pp@gmail.com>
2024-02-04 20:48:17 +0300

    new81

---
+++ roles/deploy_apache_web/files/uber_pr/js/inter.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+    subheder_btn_z = document.querySelector('.subheder_btn_z');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+    
+})
